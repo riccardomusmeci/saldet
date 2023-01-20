1794f904e5a28fe716f57406255056c2f621f0da
Riccardo Musmeci <riccardomusmeci@MacBook-Pro-di-Riccardo.local>
2023-01-20 13:50:06 +0100

    added saliency detection base dataset

---
+++ lightning_saliency/dataset/saliency.py
@@ -0,0 +1,92 @@
+import os
+import torch
+from tqdm import tqdm
+from torch.utils.data import Dataset
+from typing import List, Tuple, Callable
+from lightning_saliency.utils import to_tensor_format
+from lightning_saliency.io import read_rgb, read_binary
+
+class SaliencyDataset(Dataset):
+    
+    def __init__(
+        self,
+        root_dir: str,
+        transform: Callable = None
+    ) -> None:
+        """Saliency Detection base dataset (with images and masks dir)
+
+        Args:
+            root_dir (str): dataset dir with images and masks dirs in it
+            transform (Callable, optional): data augmentations. Defaults to None.
+        """
+        super().__init__()
+        
+        assert os.path.exists(root_dir), f"{root_dir} does not exists."
+        assert "images" in os.listdir(root_dir), f"{root_dir} must have 'images' folder in it."
+        assert "masks" in os.listdir(root_dir), f"{root_dir} must have 'masks' folder in it."
+        
+        self.images_dir = os.path.join(root_dir, "images")
+        self.masks_dir = os.path.join(root_dir, "masks")
+        
+        # it's a list of dict with {"image": path, "mask": path}
+        self.images, self.masks = self._load_data()
+        
+        self.transform = transform
+        
+        print(f"#"*12 + " Dataset Recap " + "#"*12)
+        print(f"\t> Root Dir: {root_dir}")
+        print(f"\t> Images: {len(self.images)}")
+        print(f"\t> Masks: {len(self.masks)}")
+        print(f"#"*40)
+        
+    def _load_data(
+        self
+    ) -> Tuple[List[str], List[str]]:
+        """loads data from images dir and masks dir (checks if an image has its mask)
+
+        Returns:
+            Tuple[List[str], List[str]]: images and masks paths
+        """
+        
+        _images = [f for f in os.listdir(self.images_dir)]
+        _masks = [f for f in os.listdir(self.masks_dir)]
+        
+        images, masks = [], []
+        print(f"> Loading images and masks")
+        for image in tqdm(_images):
+            image_no_ext = os.path.splitext(image)[0]
+            for mask in _masks:
+                if os.path.splitext(mask)[0] == image_no_ext:
+                    images.append(os.path.join(self.images_dir, image))
+                    masks.append(os.path.join(self.masks_dir, mask))
+                    _masks.remove(mask)
+                    break
+        
+        if len(_images) != len(images):
+            print(f"[WARNING] Found {len(_images)-len(images)} images with no masks. Removing them from dataset.")    
+        return images, masks
+        
+    def __getitem__(
+        self, 
+        index: int
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """returns (image, mask) with augmentations applied at given indec
+
+        Args:
+            index (int): index
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: (image, mask)
+        """
+        
+        image = read_rgb(self.images[index])
+        mask = read_binary(self.masks[index])
+        
+        if self.transform:
+            image, mask = self.transform(image=image, mask=mask)
+        
+        image = torch.from_numpy(to_tensor_format(image))
+        mask = torch.from_numpy(mask).long()
+        mask = mask.unsqueeze(dim=0)
+        
+        return image, mask
