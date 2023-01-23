bb4c44b49968203c34c997ff81f1329d149f4242
Riccardo Musmeci <riccardomusmeci@MBP-di-Riccardo.home-life.hub>
2023-01-23 09:06:58 +0100

    custom seed

--- saldet/utils/seed.py
+++ saldet/utils/seed.py
@@ -8,6 +8,6 @@ def seed_everything(seed):
 	os.environ['PYTHONHASHSEED'] = str(seed)
 	np.random.seed(seed)
 	torch.manual_seed(seed)
-	# torch.cuda.manual_seed_all(seed)
-	# torch.backends.cudnn.deterministic = True
-	# torch.backends.cudnn.benchmark = False
+	torch.cuda.manual_seed_all(seed)
+	torch.backends.cudnn.deterministic = True
+	torch.backends.cudnn.benchmark = False
