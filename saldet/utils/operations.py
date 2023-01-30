56411f40a708ef268b19839007b289c76d82cda2
Riccardo Musmeci <riccardomusmeci@MacBook-Pro-di-Riccardo.local>
2023-01-30 10:14:07 +0100

    normalize pred

--- saldet/utils/operations.py
+++ saldet/utils/operations.py
@@ -2,6 +2,14 @@ import torch
 import torch.nn.functional as F
 
 def flat(mask: torch.Tensor) -> torch.tensor:
+    """flats a mask
+
+    Args:
+        mask (torch.Tensor): input mask
+
+    Returns:
+        torch.tensor: flattened mask
+    """
     batch_size = mask.shape[0]
     h = 28
     mask = F.interpolate(mask,size=(int(h),int(h)), mode='bilinear')
@@ -10,3 +18,17 @@ def flat(mask: torch.Tensor) -> torch.tensor:
     g = x @ x.transpose(-2,-1) # b 28*28 28*28
     g = g.unsqueeze(1) # b 1 28*28 28*28
     return g
+
+def normalize(x: torch.Tensor) -> torch.Tensor:
+    """normalizes a tensor based on max and min of tensor
+
+    Args:
+        x (torch.Tensor): input tensor
+
+    Returns:
+        torch.Tensor: normalized tensor
+    """
+    t_max = torch.max(x)
+    t_min = torch.min(x)
+    out = (x-t_min)/(t_max-t_min)
+    return out
