564e491cced17cccd626261230e15b5a6c0e582a
Riccardo Musmeci <riccardomusmeci92@gmail.com>
2023-05-10 22:47:53 +0200

    fix mps mmb

--- saldet/ops/tensor.py
+++ saldet/ops/tensor.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn.functional as F
 
+from saldet.utils import device
+
 
 def flat(mask: torch.Tensor) -> torch.tensor:
     """Flat a mask
@@ -16,6 +18,8 @@ def flat(mask: torch.Tensor) -> torch.tensor:
     mask = F.interpolate(mask, size=(int(h), int(h)), mode="bilinear")
     x = mask.view(batch_size, 1, -1).permute(0, 2, 1)
     # print(x.shape)  b 28*28 1
+    if device() == "mps":
+        x = x.float()
     g = x @ x.transpose(-2, -1)  # b 28*28 28*28
     g = g.unsqueeze(1)  # b 1 28*28 28*28
     return g
