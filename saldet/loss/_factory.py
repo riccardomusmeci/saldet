d1156cbdb9cada565cc29ddb21b6b0a5568b7890
Riccardo Musmeci <riccardomusmeci92@gmail.com>
2023-06-02 20:45:06 +0200

    add EdgeSaliencyLoss for PFAN

--- saldet/loss/_factory.py
+++ saldet/loss/_factory.py
@@ -1,11 +1,17 @@
 import torch.nn as nn
 
+from saldet.loss.edge_saliency_loss import EdgeSaliencyLoss
 from saldet.loss.multi_bce import MultiBCELoss
 from saldet.loss.pg import PGLoss
 
 __all__ = ["create_criterion"]
 
-FACTORY = {"multibce": MultiBCELoss, "bce": nn.BCELoss, "pg": PGLoss}
+FACTORY = {
+    "multibce": MultiBCELoss,
+    "bce": nn.BCELoss,
+    "pg": PGLoss,
+    "pfan": EdgeSaliencyLoss,
+}
 
 
 def create_criterion(criterion: str, **kwargs) -> nn.Module:
